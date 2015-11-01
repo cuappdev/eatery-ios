9fbd9fa481340d01b4b4382b465054b0103e5526
Alexander Zielenski <alex@alexzielenski.com>
2015-11-01 14:40:05 -0500

    re-add favorites feature

--- Classes/Data/Eatery+Extensions.swift
+++ Classes/Data/Eatery+Extensions.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import DiningStack
+
+extension Eatery {
+    //!TODO: Maybe cache this value? I don't think this is too expensive
+    var favorite: Bool {
+        get {
+            let ar = NSUserDefaults.standardUserDefaults().arrayForKey("favorites") ?? []
+            return ar.contains({ [unowned self] (x) -> Bool in
+                return x as? String == self.slug
+                })
+        }
+        
+        set {
+            var ar = NSUserDefaults.standardUserDefaults().arrayForKey("favorites") ?? []
+            let contains = self.favorite
+            if (newValue && !contains) {
+                ar.append(self.slug)
+            } else if (!newValue && contains) {
+                let idx = ar.indexOf({ [unowned self] (obj) -> Bool in
+                    return obj as? String == self.slug
+                    })
+                
+                if let idx = idx {
+                    ar.removeAtIndex(idx)
+                }
+            }
+            
+            NSUserDefaults.standardUserDefaults().setObject(ar, forKey: "favorites");
+        }
+    }
+}
