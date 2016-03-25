98b6be176194d361f1d15e5d96d8b0a0d9ef1a54
Logan Allen <logan.allen@sbcglobal.net>
2016-03-25 16:42:10 -0400

    Removed commented code

--- Classes/Eat Now/Detail View Controller/TableView Cells/EateryHeaderTableViewCell.swift
+++ Classes/Eat Now/Detail View Controller/TableView Cells/EateryHeaderTableViewCell.swift
@@ -26,7 +26,6 @@ class EateryHeaderTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         selectionStyle = .None
-//        infoButton.tintColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5)
     }
     
     override func didMoveToWindow() {
