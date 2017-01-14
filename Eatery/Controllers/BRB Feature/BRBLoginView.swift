c83ef4d113d74217f228215963182d63888699c2
Dennis Fedorko <dennis.fedorko@gmail.com>
2017-01-14 03:43:39 -0500

    Update UI for login progress feedback

--- Eatery/Controllers/BRB Feature/BRBLoginView.swift
+++ Eatery/Controllers/BRB Feature/BRBLoginView.swift
@@ -35,13 +35,13 @@ class BRBLoginView: UIView, UITextFieldDelegate {
         
         privacyStatementTextView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
         privacyStatementTextView.isEditable = false
-        privacyStatementTextView.text = "\n\nWhen you log in using our system, we will use your credentials solely to fetch your account information on your behalf. Your credentials will be stored safely on this device in a manner similar to how a web browser might cache your login information.\n\nYour netid and password will never leave your device, and will never be stored on our servers or viewed by anyone on our team.\n\nYou may log out of your account at any time to erase all of your account information from this device."
-        privacyStatementTextView.textAlignment = .justified
+        privacyStatementTextView.text = "\n\nPrivacy Statement\n\n\nWhen you log in using our system, we will use your credentials solely to fetch your account information on your behalf. Your credentials will be stored safely on this device in a manner similar to how a web browser might cache your login information.\n\nYour netid and password will never leave your device, and will never be stored on our servers or viewed by anyone on our team.\n\nYou may log out of your account at any time to erase all of your account information from this device.\n\n\nTap Anywhere To Dismiss"
+        privacyStatementTextView.textAlignment = .center
         privacyStatementTextView.font = UIFont.systemFont(ofSize: 14)
         privacyStatementTextView.backgroundColor = UIColor(white: 0.93, alpha: 1)
         privacyStatementTextView.textContainerInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
         
-        headerLabel.frame = CGRect(x: 0, y: frame.width * 0.08, width: frame.width * 0.8, height: 40)
+        headerLabel.frame = CGRect(x: 0, y: frame.width * 0.07, width: frame.width * 0.8, height: 40)
         headerLabel.center = CGPoint(x: frame.width / 2.0, y: headerLabel.center.y)
         headerLabel.text = "Log in with your Cornell NetID to see your account balance and history"
         headerLabel.numberOfLines = 2
@@ -110,6 +110,7 @@ class BRBLoginView: UIView, UITextFieldDelegate {
         perpetualLoginButton.titleLabel?.font = UIFont.systemFont(ofSize: 12)
         perpetualLoginButton.titleLabel?.textAlignment = .left
         perpetualLoginButton.addTarget(self, action: #selector(BRBLoginView.keepMeSignedIn), for: .touchUpInside)
+        perpetualLoginButton.sendActions(for: .touchUpInside)
         addSubview(perpetualLoginButton)
         
         loginButton.frame = CGRect(x: 20, y: perpetualLoginButton.frame.maxY + 20, width: frame.width - 40, height: 55)
@@ -118,7 +119,7 @@ class BRBLoginView: UIView, UITextFieldDelegate {
         loginButton.setBackgroundImage(UIImage.image(withColor: .black), for: .highlighted)
         loginButton.titleLabel?.textAlignment = .center
         loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 15)
-        loginButton.addTarget(self, action: #selector(BRBLoginView.login), for: .touchUpInside)
+        loginButton.addTarget(self, action: #selector(login), for: .touchUpInside)
         addSubview(loginButton)
         
         activityIndicator = UIActivityIndicatorView()
@@ -154,11 +155,18 @@ class BRBLoginView: UIView, UITextFieldDelegate {
     }
     
     func login() {
+        
         let netid = (netidTextField.text ?? "").lowercased()
         let password = passwordTextField.text ?? ""
         
         if netid.characters.count > 0 && password.characters.count > 0 {
+            
+            if headerLabel.alpha != 1.0 {
+                animateToAdjustToKeyboard(keyboardIsDisplaying: true)
+            }
+
             headerLabel.text = ""
+            privacyStatementButton.setTitle("Logging in, this may take a minute", for: .normal)
             activityIndicator.startAnimating()
             
             delegate?.brbLoginViewClickedLogin(brbLoginView: self, netid: netid, password: password)
@@ -178,6 +186,7 @@ class BRBLoginView: UIView, UITextFieldDelegate {
     func loginFailedWithError(error: String) {
         headerLabel.textColor = .red
         headerLabel.text = error
+        privacyStatementButton.setTitle("Privacy Statement", for: .normal)
         activityIndicator.stopAnimating()
         
         isUserInteractionEnabled = true
--- Eatery/Controllers/BRB Feature/BRBViewController.swift
+++ Eatery/Controllers/BRB Feature/BRBViewController.swift
@@ -21,6 +21,7 @@ class BRBViewController: UIViewController, BRBConnectionErrorHandler, BRBLoginVi
     var hasLoadedMore = 0.0
     var paginationCounter = 0
     let activityIndicatorView = UIActivityIndicatorView()
+    let activityIndicatorDescriptionLabel = UILabel()
     let timeout = 15.0 // seconds
     var time = 0.0 // time of request
     var historyHeader : EateriesCollectionViewHeaderView?
@@ -49,6 +50,12 @@ class BRBViewController: UIViewController, BRBConnectionErrorHandler, BRBLoginVi
         activityIndicatorView.color = .black
         activityIndicatorView.hidesWhenStopped = true
         
+        activityIndicatorDescriptionLabel.frame = CGRect(x: 0, y: activityIndicatorView.frame.maxY + 10, width: view.frame.width * 0.8, height: 20)
+        activityIndicatorDescriptionLabel.center = CGPoint(x: view.frame.width / 2.0, y: activityIndicatorDescriptionLabel.center.y)
+        activityIndicatorDescriptionLabel.text = "Logging in, this may take a minute"
+        activityIndicatorDescriptionLabel.textAlignment = .center
+        activityIndicatorDescriptionLabel.font = UIFont.systemFont(ofSize: 12)
+        
         if connectionHandler.accountBalance != nil // already logging in
         {
             finishedLogin()
@@ -68,6 +75,7 @@ class BRBViewController: UIViewController, BRBConnectionErrorHandler, BRBLoginVi
             {
                 activityIndicatorView.startAnimating()
                 view.addSubview(activityIndicatorView)
+                view.addSubview(activityIndicatorDescriptionLabel)
                 
                 timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(BRBViewController.timer(timer:)), userInfo: nil, repeats: true)
             }
@@ -325,6 +333,7 @@ class BRBViewController: UIViewController, BRBConnectionErrorHandler, BRBLoginVi
         }
 
         activityIndicatorView.stopAnimating()
+        activityIndicatorDescriptionLabel.removeFromSuperview()
         
         if loginView != nil {
             loginView.removeFromSuperview()
