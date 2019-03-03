f00488d681b3fb1f3a116da875e54e34706d8db2
Gonzalo Gonzalez <gonzalogonzalez2000@gmail.com>
2019-03-03 18:04:24 -0500

    Create appropriate Swift files
    Mimic the Cornell eatery files but appended with CTown

---
+++ Eatery/Controllers/Eateries/CTownMenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CTownMenuViewController.swift
+//  Eatery
+//
+//  Created by Gonzalo Gonzalez on 3/3/19.
+//  Copyright © 2019 CUAppDev. All rights reserved.
+//
+
+import UIKit
+
+class CTownMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Eatery/Views/Eateries/CTownMenuHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  CTownMenuHeaderView.swift
+//  Eatery
+//
+//  Created by Gonzalo Gonzalez on 3/3/19.
+//  Copyright © 2019 CUAppDev. All rights reserved.
+//
+
+import UIKit
+
+class CTownMenuHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
