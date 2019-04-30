81c9518a1f495e855275639ed75ff719c2d66d65
Ethan Fine <usubzero@gmail.com>
2019-04-29 22:38:53 -0400

    Completes histogram generation

--- Eatery/HistogramTestViewController.swift
+++ Eatery/HistogramTestViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class HistogramTestViewController: UIViewController {
     
-    var histogram: WaitTimeHistogram!
-    
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -21,18 +19,18 @@ class HistogramTestViewController: UIViewController {
     }
     
     override func viewDidLoad() {
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .white
         
-        let frame = CGRect(x: 30, y: 30, width: 100, height: 100)
+        let frame = CGRect(x: 10, y: 30, width: 350, height: 150)
         let waitTimeHistogram = HistogramViewController(frame: frame, data: [(2, 4), (4, 6), (3, 7), (5, 7), (1, 3), (7, 9)])
         addChildViewController(waitTimeHistogram)
         view.addSubview(waitTimeHistogram.view)
         waitTimeHistogram.didMove(toParentViewController: self)
-        //histogram = WaitTimeHistogram(frame: frame, data: [4, 5, 7, 8, 9, 10])
         
         waitTimeHistogram.view.snp.makeConstraints { make in
-            make.width.height.equalTo(100)
-            make.top.leading.equalTo(view).offset(30)
+            make.width.equalTo(350)
+            make.height.equalTo(150)
+            make.top.leading.equalTo(view).offset(10)
         }
     }
     
--- Eatery/Views/Eateries/HistogramViewController.swift
+++ Eatery/Views/Eateries/HistogramViewController.swift
@@ -10,23 +10,31 @@ import UIKit
 
 class HistogramViewController: UIViewController {
     
+    // MARK: Data
     var data: [(minWait: Int, maxWait: Int)]!
     // hours in military time units
-    let hourRange = (startTime: 0, endTime: 6)
+    let hourRange = (startTime: 6, endTime: 3)
+    var overflowingEndTime: Int!
     
+    // MARK: Views and view properties
     var viewFrame: CGRect!
     var axisView: UIView!
     var axisTickViews = [UIView]()
     var axisTickLabels = [UILabel]()
     
+    var barWidth: Double!
     var barContainerView: UIView!
     var barViews = [UIView]()
     var barStackView: UIStackView!
     
+    // MARK: Initializers
+    
     init(frame: CGRect, data: [(Int, Int)]) {
         super.init(nibName: nil, bundle: nil)
+        
         self.viewFrame = frame
         self.data = data
+        overflowingEndTime = (hourRange.endTime < hourRange.startTime) ? hourRange.endTime + 24 : hourRange.endTime
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -36,83 +44,142 @@ class HistogramViewController: UIViewController {
     override func viewDidLoad() {
         view.frame = viewFrame
         
-        setUpAxis()
         setUpBarViews()
+        setUpAxis()
+    }
+    
+    // MARK: View set up
+    
+    private func setUpAxisLineView() {
+        let axisViewFrame = CGRect(x: 0, y: 35, width: Int(view.frame.width), height: 2)
+        let axisView = UIView(frame: axisViewFrame)
+        axisView.backgroundColor = .lightGray
+        view.addSubview(axisView)
+        
+        axisView.snp.makeConstraints { make in
+            make.height.equalTo(2)
+            make.width.equalTo(view)
+            make.bottom.equalTo(view).inset(45)
+            make.leading.trailing.equalTo(view)
+        }
     }
     
     private func setUpAxis() {
-        let tickCount: Int = (hourRange.endTime - hourRange.startTime) / 3
+        setUpAxisLineView()
+        
+        let overflowingEndTime = (hourRange.endTime < hourRange.startTime) ? hourRange.endTime + 24 : hourRange.endTime
+        let tickCount: Int = ((overflowingEndTime - hourRange.startTime) / 3) + 1
+        var x: Double = 0
         for tick in 0..<tickCount {
-            let x = tick * Int(view.frame.width) / tickCount
-            let tickFrame = CGRect(x: x, y: 10, width: 2, height: 10)
+            let tickFrame = CGRect(x: x, y: 35, width: 2, height: 10)
             let tickView = UIView(frame: tickFrame)
             axisTickViews.append(tickView)
             view.addSubview(tickView)
             
-            tickView.backgroundColor = .black
+            tickView.backgroundColor = .lightGray
             tickView.snp.makeConstraints { make in
-                make.bottom.equalTo(view).inset(10)
+                make.bottom.equalTo(view).inset(35)
                 make.leading.equalTo(view).offset(x)
                 make.width.equalTo(2)
                 make.height.equalTo(10)
             }
             
-            let tickLabelFrame = CGRect(x: x, y: 0, width: 10, height: 10)
+            let tickLabelFrame = CGRect(x: x, y: 10, width: 30, height: 20)
             let tickLabel = UILabel(frame: tickLabelFrame)
-            let tickHour = hourRange.startTime + tick
-            print(tickHour)
-            let tickHourPostfix = tickHour >= 12 ? "p" : "a"
-            print(tickHourPostfix)
-            tickLabel.text = "\(hourRange.startTime + tick) \(tickHourPostfix)"
+            let tickHour = hourRange.startTime + (tick * 3)
+            tickLabel.text = formattedHourForTime(overflowedMilitaryHour: tickHour)
+            tickLabel.textColor = .darkGray
             view.addSubview(tickLabel)
             
             tickLabel.snp.makeConstraints { make in
-                make.bottom.equalTo(view)
+                make.bottom.equalTo(view).inset(10)
                 make.leading.equalTo(view).offset(x)
-                make.width.height.equalTo(10)
+                make.width.equalTo(30)
+                make.height.equalTo(15)
             }
+            
+            let additionalSpacing = !(tick == 0 || tick == (tickCount - 2))
+            let xSpacingAdd: Double = (((additionalSpacing ? 2 : 0) + 1) * 2)
+            x += (3 * barWidth) + xSpacingAdd
         }
     }
     
-    private func setUpBarViews() {
-        let barContainerViewFrame = CGRect(x: 0, y: 30, width: view.frame.width, height: view.frame.height - 70)
+    private func setUpBarContainerView() {
+        let barContainerViewFrame = CGRect(x: 0, y: 50, width: view.frame.width, height: view.frame.height - 70)
         barContainerView = UIView(frame: barContainerViewFrame)
         view.addSubview(barContainerView)
         
         barContainerView.snp.makeConstraints { make in
             make.leading.equalTo(view)
-            make.bottom.equalTo(view).inset(30)
+            make.bottom.equalTo(view).inset(47)
             make.width.equalTo(view)
-            make.height.equalTo(view).inset(70)
+            make.height.equalTo(view).inset(65)
         }
+    }
+    
+    private func setUpBarViews() {
+        setUpBarContainerView()
         
-        let barCount = hourRange.endTime - hourRange.startTime
+        let barCount = (overflowingEndTime - hourRange.startTime) - 2
+        let frameWidth = Double(view.frame.width)
+        let spacingLoss = Double(2 * (barCount - 1))
+        barWidth = (frameWidth - spacingLoss) / Double((barCount + 2))
         for barIndex in 0..<barCount {
-            var barWidth = Int(view.frame.width) / barCount + 2
-            barWidth = barWidth - (((barCount - 1) * 3) / barCount)
-            let barHeight = Int.random(in: 0...1) * 20
-            let x = (barIndex == 0) ? barWidth : Int(barViews[barIndex - 1] .frame.maxX)
-            let barFrame = CGRect(x: x, y: 0, width: barWidth, height: barHeight)
+            let barHeight = Double.random(in: 0...1) * 50
+            let x = (barIndex == 0) ? barWidth : Double(barViews[barIndex - 1] .frame.maxX)
+            let barFrame = CGRect(x: x!, y: 0, width: barWidth, height: barHeight)
             let barView = UIView(frame: barFrame)
+            roundBarViewCorners(barView: barView)
             barViews.append(barView)
             barContainerView.addSubview(barView)
             
-            barView.backgroundColor = .eateryBlue
             barView.snp.makeConstraints { make in
                 if barIndex == 0 {
                     make.leading.equalTo(view).offset(barWidth)
                 } else {
-                    make.leading.equalTo(barViews[barIndex - 1].snp.trailing).offset(3)
+                    make.leading.equalTo(barViews[barIndex - 1].snp.trailing).offset(2)
                 }
-                /*if barIndex == barCount - 1 {
-                    make.trailing.equalTo(view).inset(barWidth)
-                }*/
-                make.bottom.equalTo(view).inset(40)
                 
+                make.bottom.equalTo(barContainerView)
                 make.width.equalTo(barWidth)
                 make.height.equalTo(barHeight)
             }
         }
+    }
+    
+    // MARK: Utility functions for set up
+    
+    private func roundBarViewCorners(barView: UIView) {
+        if #available(iOS 11.0, *){
+            barView.backgroundColor = .histogramBarBlue
+            barView.clipsToBounds = false
+            barView.layer.cornerRadius = 3
+            barView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        } else{
+            let barShape = CAShapeLayer()
+            barShape.bounds = barView.frame
+            barShape.position = barView.center
+            barShape.path = UIBezierPath(roundedRect: view.bounds, byRoundingCorners: [.topLeft , .topRight], cornerRadii: CGSize(width: 3, height: 3)).cgPath
+            barView.layer.backgroundColor = UIColor.green.cgColor
+            barView.layer.mask = barShape
+        }
+    }
+    
+    // Overflowed military hour is a time between 0...47
+    private func formattedHourForTime(overflowedMilitaryHour: Int) -> String {
+        var formattedHour: String!
+        if overflowedMilitaryHour >= 24 {
+            return formattedHourForTime(overflowedMilitaryHour: overflowedMilitaryHour - 24)
+        } else if overflowedMilitaryHour > 12 {
+            formattedHour = "\(overflowedMilitaryHour - 12)p"
+        } else if overflowedMilitaryHour == 12 {
+            formattedHour = "12p"
+        } else if overflowedMilitaryHour > 0 {
+            formattedHour = "\(overflowedMilitaryHour)a"
+        } else {
+            formattedHour = "12a"
+        }
+        return formattedHour
     }
     
     private func expandWaitTime(barIndex: Int, barView: UIView) {
