26b40c916dc695ed8be615ca9fe71f97adbaee6d
William Ma <whoiswillma@gmail.com>
2019-11-20 21:11:24 -0500

    Update Kingfisher+Eatery.swift

--- Eatery/Extensions/Kingfisher+Eatery.swift
+++ Eatery/Extensions/Kingfisher+Eatery.swift
@@ -13,7 +13,8 @@ extension Kingfisher where Base == UIImageView {
     func setImage(with eatery: Eatery) {
         let placeholder = UIImage.image(withColor: UIColor(white: 0.97, alpha: 1.0))
         if let url = eatery.imageUrl {
-            setImage(with: url, placeholder: placeholder, options: [.transition(.fade(0.35))])
+            let resource = ImageResource(downloadURL: url)
+            setImage(with: resource, placeholder: placeholder, options: [.transition(.fade(0.35))])
         }
     }
 
