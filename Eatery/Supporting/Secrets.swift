fb665c3443de1bc153cd97f7f9aa0e926f503abd
Kevin Chan <kevinchan159@gmail.com>
2020-02-22 01:09:40 -0500

    Fixes naming in Secrets.swift

--- Eatery/Supporting/Secrets.swift
+++ Eatery/Supporting/Secrets.swift
@@ -10,13 +10,13 @@ import Foundation
 
 struct Secrets {
 
-    static let announcementsCommonPath = Secrets.keyDict["announcements-common-path"] as! String
-    static let announcementsHost = Secrets.keyDict["announcements-host"] as! String
-    static let announcementsPath = Secrets.keyDict["announcements-path"] as! String
-    static let announcementsScheme = Secrets.keyDict["announcements-scheme"] as! String
+    static let announcementsCommonPath = Secrets.secretDict["announcements-common-path"] as! String
+    static let announcementsHost = Secrets.secretDict["announcements-host"] as! String
+    static let announcementsPath = Secrets.secretDict["announcements-path"] as! String
+    static let announcementsScheme = Secrets.secretDict["announcements-scheme"] as! String
 
-    private static let keyDict: NSDictionary = {
-        guard let path = Bundle.main.path(forResource: "Keys", ofType: "plist"),
+    private static let secretDict: NSDictionary = {
+        guard let path = Bundle.main.path(forResource: "Secrets", ofType: "plist"),
             let dict = NSDictionary(contentsOfFile: path) else { return [:] }
         return dict
     }()
