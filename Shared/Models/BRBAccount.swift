7e6def790b6cb5aabc3a7694ac43e3b29c646b6a
William Ma <whoiswillma@gmail.com>
2020-08-30 14:03:05 -0400

    BRBAccount: Add fake account for app store review

--- Shared/Models/BRBAccount.swift
+++ Shared/Models/BRBAccount.swift
@@ -10,6 +10,51 @@ import Foundation
 
 struct BRBAccount: Codable {
 
+    static let fakeAccount = BRBAccount(
+        cityBucks: "20.00",
+        laundry: "15.95",
+        brbs: "1,000.00",
+        swipes: "None",
+        history: [
+            BRBHistory(
+                name: "Rusty\'s",
+                timestamp: "Friday, Mar 13 at 10:40 AM",
+                amount: "2.69",
+                positive: false
+            ),
+            BRBHistory(
+                name: "Jansen\'s Market",
+                timestamp: "Friday, Mar 13 at 12:30 AM",
+                amount: "2.49",
+                positive: false
+            ),
+            BRBHistory(
+                name: "Libe Café",
+                timestamp: "Thursday, Mar 12 at 09:09 PM",
+                amount: "2.29",
+                positive: false
+            ),
+            BRBHistory(
+                name: "Libe Café",
+                timestamp: "Thursday, Mar 12 at 04:14 PM",
+                amount: "1.84",
+                positive: false
+            ),
+            BRBHistory(
+                name: "Mattin\'s Café",
+                timestamp: "Wednesday, Mar 11 at 04:23 PM",
+                amount: "1.89",
+                positive: false
+            ),
+            BRBHistory(
+                name: "Jansen\'s Market",
+                timestamp: "Tuesday, Mar 10 at 08:15 PM",
+                amount: "2.29",
+                positive: false
+            )
+        ]
+    )
+
     var cityBucks: String
     var laundry: String
     var brbs: String
